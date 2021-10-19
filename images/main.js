266dc9b9e7569077ccd7b37892be317cfa325f8f
Gagan-Chaudhary <gaganveermaan@gmail.com>
2021-10-19 22:49:57 +0530

    make it better

---
+++ images/main.js
@@ -0,0 +1,17 @@
+// const submitBtn = document.getElementById('submitBtn');
+
+// const getInfo = (event) => {
+//   event.preventDefault();
+  
+//   // let url=api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key};
+//   if(false){
+
+//   }
+//   else{
+//     let url=`https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=2b04db940461a4da116259f5a8b82a65`;
+//     const response = fetch(url);
+//     console.log(response)
+//   }
+// };
+
+// submitBtn.addEventListener('click', getInfo);
--- index.js
+++ index.js
@@ -2,25 +2,41 @@ const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
+
+// for using static files
+const staticPath = path.join(__dirname, "/images");
+const template_path = path.join(__dirname, "/templates/views");
+const partials_path = path.join(__dirname, "/templates/partials");
+app.use(express.static(staticPath));
+// console.log(staticPath);
 
 app.set("view engine", "hbs");
-// app.set("view")
+app.set("views", template_path);
+// app.use(express.static("images"));
+hbs.registerPartials(partials_path);
 
 app.set();
-app.get("/", (req, res) => {
+
+// Routing
+// Routing
+
+app.get("", (req, res) => {
   // console.log("Home page");
-  res.send("Home page");
-  res.render("index", () => {
-    // var inp = document.getElementById("input").value; console.log(inp);
-  });
+  // res.send("Home page");
+  res.render("index");
+  // console.log(req.query);
+  req.query = "/red";
 });
 
 app.get("/about", (req, res) => {
-  res.send("welcome to About Page ");
+  // res.send("welcome to About Page of the website ");
+  res.render("about");
 });
 app.get("/weather", (req, res) => {
-  res.send("Checkout weather ");
+  // res.send("Checkout weather ");
+  // res.render("weather");
+  res.render("test");
 });
 app.get("*", (req, res) => {
   res.send("Page do not exist ");
